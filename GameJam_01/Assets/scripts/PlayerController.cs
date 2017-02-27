bf79f868ea0e05247359fbc27a6481c25e9c08b1
Tyler Nunez <W0532191@selu.edu>
2017-02-27 03:04:00 -0600

    Removed additional standard assets that will not be used. Established the basic armor/weakpoint system, still needs to be tweeked as the capsule collider causes an error in detection. Will look for more tutorials. Created a small test area with two player prefabs.

---
+++ GameJam_01/Assets/scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 6.0F;
+    public bool sprint = false;
+    public Collider other;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        float verticalMovement = Input.GetAxis("Vertical") * speed;
+        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        verticalMovement *= Time.deltaTime;
+        horizontalMovement *= Time.deltaTime;
+
+        float angleH = Input.GetAxis("RightH") * 10;
+        transform.localEulerAngles = new Vector3(0, angleH, 0);
+
+        transform.Translate(horizontalMovement, 0, verticalMovement);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        //Constantly checking if the collider is a player
+        if(Input.GetButtonDown("Fire1"))
+        {
+            attack();
+        } 
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        //Target is in range to be attacked
+        other = col;
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        //Target is not in range to be attacked
+        other = null;
+    }
+    public void attack()
+    {
+        if (other != null)
+        {
+            if (other.transform.parent.tag != null)
+            {
+                if (other.transform.parent.tag == "Player")
+                {
+                    //Should check the collider tag to see if the collider is the armor or vulnerability
+                    if (other.gameObject.tag == "Armor")
+                    {
+                        Debug.Log("You hit armor!");
+                    }
+                    if (other.gameObject.tag == "Weakness")
+                    {
+                        Debug.Log("You hit the weak point!");
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+      
+    
+
+}
+
