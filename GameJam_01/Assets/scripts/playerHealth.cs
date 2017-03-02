a3c092ace38a3812b8e6ff347a809a8faa2c0560
Tyler Nunez <tylernunez57@gmail.com>
2017-03-02 02:08:35 -0600

    Added model to scene, worked out hitboxes and added directory for animations.

---
+++ GameJam_01/Assets/scripts/playerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+
+    public int health;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void damage()
+    {
+        --this.health;
+    }
+
+}
